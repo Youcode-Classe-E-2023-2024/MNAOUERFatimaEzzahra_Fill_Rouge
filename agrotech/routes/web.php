c7ba0b290ed6bf094c63c451066db5371929e7b7
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-04-02 16:18:35 +0000

    Update web

--- agrotech/routes/web.php
+++ agrotech/routes/web.php
@@ -9,7 +9,9 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\SubscriberController;
 use App\Http\Controllers\TagController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,31 +25,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+/* ----Home---- */
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
+
+/* ----Register---- */
 Route::get('/register', [RegisterController::class, 'create'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
+
+/* ----Login---- */
 Route::get('/login', [LoginController::class, 'create'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 
+
+/* ----Forgot password---- */
 Route::get('/forgot-password', [ForgotPasswordController::class, 'create'])->name('password.create');
 Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])->name('password.store');
 Route::get('/forgot-password/{token}', [ForgotPasswordController::class, 'edit'])->name('password.reset');
 Route::post('/forgot-password/reset', [ForgotPasswordController::class, 'update'])->name('password.update');
 
-Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 
+/* ----Manager Article---- */
+Route::get('/Article', [ArticleController::class, 'index'])->name('admin.article');
+Route::get('/createArticle', [ArticleController::class, 'create'])->name('create.article');
 Route::get('/articleDetail', [ArticleDetailController::class, 'create'])->name('Detail.article');
-
 Route::get('/favorite', [ArticleDetailController::class, 'index'])->name('favorite.article');
 
-Route::get('/createArticle', [ArticleController::class, 'create'])->name('create.article');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+/* ----Manager User---- */
+Route::get('/user', [UserController::class, 'index'])->name('user');
 
-Route::get('/profile', [ProfilController::class, 'index'])->name('profile');
 
 /* ----Manager category---- */
 Route::get('/category', [CategoryController::class, 'index'])->name('category');
@@ -55,11 +66,21 @@ Route::post('/category/store', [CategoryController::class, 'store'])->name('cate
 Route::post('/category/update', [CategoryController::class, 'update'])->name('category.update');
 Route::post('/category/destroy', [CategoryController::class, 'destroy'])->name('category.destroy');
 
+
 /* ----Manager tag---- */
 Route::get('/tag', [TagController::class, 'index'])->name('tag');
 Route::post('/tag/store', [TagController::class, 'store'])->name('tag.store');
 Route::post('/tag/update', [TagController::class, 'update'])->name('tag.update');
 Route::post('/tag/destroy', [TagController::class, 'destroy'])->name('tag.destroy');
 
-Route::get('/Article', [ArticleController::class, 'index'])->name('admin.article');
 
+/* ---Subscriber---*/
+Route::get('/listeSubscriber' ,[SubscriberController::class, 'ListeSub'])->name('show.subscriber');
+Route::post('/unsubscribe' ,[SubscriberController::class, 'unsubscribe'])->name('unsubscribe');
+Route::get('/dashboard', [SubscriberController::class,'showSubscriberStatistics'])->name('showSubscriberStatistics');
+Route::post('/home' ,[SubscriberController::class, 'addSubscriber'])->name('add_subscriber');
+Route::get('/dashboard', [subscriberController::class,'showSubscriberStatistics'])->name('showSubscriberStatistics');
+
+
+/* ---Dashboard---*/
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
