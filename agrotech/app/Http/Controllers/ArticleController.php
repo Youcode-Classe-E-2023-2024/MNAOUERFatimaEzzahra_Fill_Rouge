c85d47b3695506e235b945554dfcd60030e9d5e5
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-04-04 16:29:15 +0000

    Update Article controller

--- agrotech/app/Http/Controllers/ArticleController.php
+++ agrotech/app/Http/Controllers/ArticleController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function create()
     {
-        return view('createArticle');
+        $cats = Category::all();
+        return view('createArticle',['cats'=>$cats]);
     }
 
 //    article backoffice
