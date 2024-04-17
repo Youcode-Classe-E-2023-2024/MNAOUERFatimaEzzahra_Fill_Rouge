00b60ab9d09b86e97e23e31e5d37629fbd27f377
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-04-17 15:41:20 +0100

    Create articles table

---
+++ agrotech/database/migrations/2024_04_17_142953_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('picture');
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('categories_id');
+            $table->unsignedBigInteger('tags_id');
+            $table->timestamps();
+
+            $table->foreign('categories_id')->references('id')->on('categories')->cascadeOnDelete();
+            $table->foreign('tags_id')->references('id')->on('tags')->cascadeOnUpdate();
+            $table->foreign('created_by')->references('id')->on('users')->cascadeOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
