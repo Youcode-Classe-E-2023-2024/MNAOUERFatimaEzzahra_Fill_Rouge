4bdb505ec62fce73799b6c54ff3bed265e4a2c34
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-04-24 23:42:50 +0100

    Update app.blade.php

--- agrotech/resources/views/layout/app.blade.php
+++ agrotech/resources/views/layout/app.blade.php
@@ -89,8 +89,8 @@
 
 @yield('content')
 <!-- Footer Start -->
-<div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5">
-    <div class="container py-0">
+<div class="container-fluid bg-dark text-white-50 footer">
+    <div class="container py-5">
         <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(226, 175, 24, 0.5) ;">
             <div class="row g-4">
                 <div class="col-lg-3">
@@ -131,10 +131,10 @@
 <!-- Footer End -->
 
 <!-- Copyright Start -->
-<div class="container-fluid copyright bg-dark py-4">
+<div class="container-fluid bg-dark">
     <div class="container">
         <div class="row">
-            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+            <div class="col-md-6 text-center text-md-start mb-md-0">
                 <span class="text-light"><a href="#"><i class="fas fa-copyright text-light me-2"></i>AGROTECH</a>, All right reserved.</span>
             </div>
             <div class="col-md-6 my-auto text-center text-md-end text-white">
