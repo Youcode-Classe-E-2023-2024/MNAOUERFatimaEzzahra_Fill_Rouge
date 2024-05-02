9885bec5bfc73f2ee52c5bb9e8b80269e8d486d1
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-05-02 10:24:26 +0100

    Add Midlleware

--- agrotech/app/Http/Middleware/Authenticate.php
+++ agrotech/app/Http/Middleware/Authenticate.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
-class Authenticate extends Middleware
+class Authenticate 
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
-    protected function redirectTo(Request $request): ?string
+    public function handle(Request $request, Closure $next)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (Auth::check()) {
+            
+            return $next($request);
+        }
+
+        return redirect()->route('login')->with('error', 'Vous devez être connecté pour accéder à cette page.');    
+        
     }
 }
